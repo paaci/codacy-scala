9cd629831836fdcae9ceb8de9fef24624f1135f7
paaci <paaci@freemail.hu>
2017-05-04 16:00:11 +0200

    modified test to ask the actor - asyncAssert

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,12 +1,15 @@
 import akka.actor.{ActorSystem, Props}
-import akka.testkit.{ImplicitSender, TestKit}
-import org.scalatest._
 import akka.pattern.ask
+import akka.testkit.{ImplicitSender, TestKit}
+import akka.util.Timeout
 import com.example.actors.WorkerActor
+import org.scalatest._
+
+import scala.concurrent.duration._
 
 class ActorSpec() extends TestKit(ActorSystem("ActorSpec"))
   with ImplicitSender
-  with WordSpecLike
+  with AsyncWordSpecLike
   with Matchers
   with BeforeAndAfterAll {
 
@@ -17,10 +20,16 @@ class ActorSpec() extends TestKit(ActorSystem("ActorSpec"))
   "WorkerActor must answer gently!" must {
 
     "answer in HTML message" in {
-      val actor = system.actorOf(Props[WorkerActor])
+
+      implicit val timeout = Timeout(5 seconds)
+
+      val testActor = system.actorOf(Props[WorkerActor])
       val testName = "John"
-      actor ! testName
-      expectMsg(s"<html><body>Akka-http web app is running on $testName!</body></html>")
+
+      testActor ? testName map {
+        case s: String => s shouldBe s"<html><body>Akka-http web app is running on $testName!</body></html>"
+      }
+
     }
 
   }
